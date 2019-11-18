70a634bd4dc83dad184e701d876e7e2233e41e72
itemka <itemka2503@gmail.com>
2019-11-19 02:03:45 +0300

    Updates

---
+++ precache-manifest.deba9892297660ec80fc248326e25d40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f45f7a478af9296be395c5dbb23ca68",
+    "url": "/ToDoList2/index.html"
+  },
+  {
+    "revision": "bd1a1f89d51b359b94c3",
+    "url": "/ToDoList2/static/css/main.649d647e.chunk.css"
+  },
+  {
+    "revision": "43a7e7ef402d593e3663",
+    "url": "/ToDoList2/static/js/2.e6b9ba67.chunk.js"
+  },
+  {
+    "revision": "bd1a1f89d51b359b94c3",
+    "url": "/ToDoList2/static/js/main.c48fd221.chunk.js"
+  },
+  {
+    "revision": "12c97a44187a9ba348a2",
+    "url": "/ToDoList2/static/js/runtime~main.a45e3aa1.js"
+  }
+]);
